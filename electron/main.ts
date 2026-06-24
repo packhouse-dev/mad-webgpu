a1e345219518d556065ba00e881e966c5530d265
packhouse-dev <packhouse.nl@gmail.com>
2026-06-25 01:54:26 +0200

    Update electron/capacitor build

--- electron/main.ts
+++ electron/main.ts
@@ -1,21 +1,21 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 720,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
+      contextIsolation: false,
+    },
   });
 
   // Load the dev server in development, or the built index.html in production
   if (process.env.NODE_ENV === 'development') {
     win.loadURL('http://localhost:3000');
   } else {
-    win.loadFile(path.join(__dirname, '../dist/index.html'));
+    win.loadFile(path.join(__dirname, 'index.html'));
   }
 }
 
